048d96ada4399d9a263d238c142a1b05f2fe9100
MehwishMeh <90473029+MehwishMeh@users.noreply.github.com>
2025-04-07 21:10:40 -0400

    Add files via upload

---
+++ Jenkinsfile.cs
@@ -0,0 +1,33 @@
+﻿pipeline {
+         agent any
+         environment {
+             DOCKERHUB_CREDENTIALS = credentials('dockerHubCredentialsId')
+         }
+         stages {
+             stage('Checkout') {
+                 steps {
+                     git 'https://dev.azure.com/mmehmoo5/MyAspNetCoreApp/_git/MyAspNetCoreApp'
+                 }
+             }
+             stage('Build Maven Project') {
+    steps {
+        sh 'mvn clean package'
+                 }
+}
+stage('Docker Login') {
+    steps {
+        sh 'echo $DOCKERHUB_CREDENTIALS | docker login -u your-dockerhub-username --password-stdin'
+                 }
+}
+stage('Docker Build') {
+    steps {
+        sh 'docker build -t your-dockerhub-username/my-app:latest .'
+                 }
+}
+stage('Docker Push') {
+    steps {
+        sh 'docker push your-dockerhub-username/my-app:latest'
+                 }
+}
+         }
+     }
